433e0e293f29410ac21672fdb38d27ad0c0eed77
Ian Unruh <ianunruh@gmail.com>
2014-03-07 12:00:55 -0600

    Initial commit
    Generated with `rails new myapp --skip-sprockets --skip-test-unit`

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Myapp::Application.config.secret_key_base = '0c789808cff16d63522c51f1721f0f610fa2d1807f439583dbdb6cca742c184a7be46f3b9624a6c79a31679c6e89eb1227857f676777c0356b1e0fb64315396f'
